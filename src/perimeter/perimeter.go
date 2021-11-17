4814d80b9e227974f01848a2e624ce4663a12c4b
zshnb <a857681664@gmail.com>
2021-11-17 13:05:03 +0800

    complete struct and interface

---
+++ src/perimeter/perimeter.go
@@ -0,0 +1,28 @@
+package perimeter
+
+type Rectangle struct {
+	width float64
+	height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Shape interface {
+	area() float64
+}
+
+const PI = 3.14
+
+func perimeter(rectangle Rectangle) float64 {
+	return (rectangle.width + rectangle.height) * 2
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return c.radius * c.radius * PI
+}
---
+++ src/perimeter/perimeter_test.go
@@ -0,0 +1,30 @@
+package perimeter
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	actual := perimeter(Rectangle{10, 10})
+	expected := 40.0
+	if actual != expected {
+		t.Errorf("expected '%.2f' but got '%.2f'", expected, actual)
+	}
+}
+
+func checkArea(t *testing.T, shape Shape, expected float64) {
+	if shape.area() != expected {
+		t.Errorf("expected '%.2f' but got '%.2f'", expected, shape.area())
+	}
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangle's area", func(t *testing.T) {
+		rectangle := Rectangle{10, 10}
+		expected := 100.0
+		checkArea(t, rectangle, expected)
+	})
+	t.Run("circle's area", func(t *testing.T) {
+		circle := Circle{1}
+		expected := 3.14
+		checkArea(t, circle, expected)
+	})
+}
